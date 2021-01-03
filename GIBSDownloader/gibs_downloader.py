b58e6493519dbc9aaa85e56f25d753caa18ad1e2
fernando <flisboa@fnal.gov>
2021-01-02 20:45:24 -0600

    Fixed PR suggestions

--- GIBSDownloader/gibs_downloader.py
+++ GIBSDownloader/gibs_downloader.py
@@ -24,11 +24,26 @@ class Handling(Enum):
     def __str__(self):
         return self.value
 
+# stores coordinate information 
+class Coordinate():
+    def __init__(self, coords: tuple):
+        """coords: (latitude, longitude)"""
+        self.x = coords[1]
+        self.y = coords[0]
+
+# stores tiling information
+class Tile():
+    def __init__(self, width, height, overlap, handling):
+        self.width = width
+        self.height = height
+        self.overlap = overlap
+        self.handling = handling
+
 ###### Helper function to calculate the proper width/height for requested image ######
 # Taken from https://github.com/NASA-IMPACT/data_share
-def calculate_width_height(extent, resolution):
+def calculate_width_height(extent: tuple, resolution: float):
     """
-    extent: [lower_latitude, left_longitude, higher_latitude, right_longitude], EG: [51.46162974683544,-22.94768591772153,53.03698575949367,-20.952234968354432]
+    extent: (upper_latitude, left_longitude, lower_latitude, right_longitude)
     resolution: represents the pixel resolution, i.e. km/pixel. Should be a value from this list: [0.03, 0.06, 0.125, 0.25, 0.5, 1, 5, 10]
     """
     KM_PER_DEG_AT_EQ = 111.
@@ -36,7 +51,7 @@ def calculate_width_height(extent, resolution):
     lons = extent[1::2]
     km_per_deg_at_lat = KM_PER_DEG_AT_EQ * np.cos(np.pi * np.mean(lats) / 180.)
     width = int((lons[1] - lons[0]) * km_per_deg_at_lat / resolution)
-    height = int((lats[1] - lats[0]) * KM_PER_DEG_AT_EQ / resolution)
+    height = int((lats[0] - lats[1]) * KM_PER_DEG_AT_EQ / resolution)
     print(width, height)
     return (width, height)
 
@@ -55,7 +70,7 @@ def _int64_feature(value):
     """Returns an int64_list from a bool / enum / int / uint."""
     return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
 
-def image_example(date, img_path, coords):
+def image_example(date: str, img_path: str, coords: tuple):
     image_raw = open(img_path, 'rb').read()
 
     tl_x = coords[0]
@@ -80,11 +95,11 @@ def image_example(date, img_path, coords):
 ###### END OF TFRECORD HELPER FUNCTIONS ######
 
 ###### MAIN DOWNLOADING FUNCTION ######
-def download_area_tiff(extent: tuple, date: str, output: str):
+def download_area_tiff(extent, date, output):
     """
     extent: (upper latitude, left longtitude, lower latitude, right longitude)
     date: YYYY-MM-DD
-    output: path/to/filename (don't specify extension)
+    output: path/to/filename (do not specify extension)
     returns tuple with dowloaded width and height
     """
     tl_y = extent[0]
@@ -92,30 +107,28 @@ def download_area_tiff(extent: tuple, date: str, output: str):
     br_y = extent[2]
     br_x = extent[3]
 
-    width, height = calculate_width_height([br_y, tl_x, tl_y, br_x], .25)
-    lat_lon = "{tx} {ty} {bx} {by}".format(tx=tl_x, ty=tl_y, bx=br_x, by=br_y)
+    width, height = calculate_width_height([tl_y, tl_x, br_y, br_x], .25)
+    lon_lat = "{tx} {ty} {bx} {by}".format(tx=tl_x, ty=tl_y, bx=br_x, by=br_y)
 
-    base = "gdal_translate -of GTiff -outsize {w} {h} -projwin {ll} '<GDAL_WMS><Service name=\"TMS\"><ServerUrl>https://gibs.earthdata.nasa.gov/wmts/epsg4326/best/MODIS_Terra_CorrectedReflectance_TrueColor/default/{d}/250m/".format(w=width, h=height, ll=lat_lon, d=date)
+    base = "gdal_translate -of GTiff -outsize {w} {h} -projwin {ll} '<GDAL_WMS><Service name=\"TMS\"><ServerUrl>https://gibs.earthdata.nasa.gov/wmts/epsg4326/best/MODIS_Terra_CorrectedReflectance_TrueColor/default/{d}/250m/".format(w=width, h=height, ll=lon_lat, d=date)
     end = "${z}/${y}/${x}.jpg</ServerUrl></Service><DataWindow><UpperLeftX>-180.0</UpperLeftX><UpperLeftY>90</UpperLeftY><LowerRightX>396.0</LowerRightX><LowerRightY>-198</LowerRightY><TileLevel>8</TileLevel><TileCountX>2</TileCountX><TileCountY>1</TileCountY><YOrigin>top</YOrigin></DataWindow><Projection>EPSG:4326</Projection><BlockSizeX>512</BlockSizeX><BlockSizeY>512</BlockSizeY><BandsCount>3</BandsCount></GDAL_WMS>' "
-    filename = output + "modis_" + date + ".tif"
+    filename = "{}modis_{}.tif".format(output, date)
     command = base + end + filename
     os.system(command)
     return filename
 
 ###### Helper function to generate file names for each tile ######
-def generate_tile_name_with_coordinates(base, x, x_min, x_size, y, y_min, y_size, tile_width, tile_height):
+def generate_tile_name_with_coordinates(date, x, x_min, x_size, y, y_min, y_size, tile):
     tl_x = x * x_size + x_min # find longitude of top left corner
     tl_y = y * y_size + y_min # find latitude of top left corner
-    br_x = (x + tile_width) * x_size + x_min # find longitude of bottom right corner
-    br_y = (y + tile_height) * y_size + y_min # find latitude of bottom right corner
-    filename = base + str(f'{round(tl_y, 4):08}') + '_' + str(f'{round(tl_x, 4):09}') + '_' + str(f'{round(br_y, 4):08}') + '_' + str(f'{round(br_x, 4):09}')
+    br_x = (x + tile.width) * x_size + x_min # find longitude of bottom right corner
+    br_y = (y + tile.height) * y_size + y_min # find latitude of bottom right corner
+    filename = "{d}_{ty}_{tx}_{by}_{bx}".format(d=date, ty=str(f'{round(tl_y, 4):08}'), tx=str(f'{round(tl_x, 4):09}'), by=str(f'{round(br_y, 4):08}'), bx=str(f'{round(br_x, 4):09}'))
     return filename
 
-
 ###### Main tiling function ######
-def img_to_tiles(tiff_path, tile_width, tile_height, overlap, output_path, handling):
+def img_to_tiles(tiff_path, tile, output_path):
     date = tiff_path[-14:-4]
-    output_base = date + "_"
         
     tif = gdal.Open(tiff_path)
     band = tif.GetRasterBand(1)
@@ -129,33 +142,33 @@ def img_to_tiles(tiff_path, tile_width, tile_height, overlap, output_path, handl
     y_min = gt[3]
     y_size = gt[5]
 
-    x_step, y_step = int(tile_width * (1 - overlap)), int(tile_height * (1 - overlap))
+    x_step, y_step = int(tile.width * (1 - tile.overlap)), int(tile.height * (1 - tile.overlap))
     x = 0 
     done_x = False
 
-    if (tile_width > WIDTH or tile_height > HEIGHT):
+    if (tile.width > WIDTH or tile.height > HEIGHT):
         raise argparse.ArgumentTypeError("Tiling dimensions greater than image dimensions")
     
     while(x < WIDTH and not done_x):
-        if(WIDTH - x < tile_width):
-            if handling == Handling.discard_incomplete_tiles:
+        if(WIDTH - x < tile.width):
+            if tile.handling == Handling.discard_incomplete_tiles:
                 done_x = True
                 continue
-            if handling == Handling.complete_tiles_shift:
-                x = WIDTH - tile_width
+            if tile.handling == Handling.complete_tiles_shift:
+                x = WIDTH - tile.width
             done_x = True
         done_y = False
         y = 0
         while (y < HEIGHT and not done_y):
-            if(HEIGHT - y < tile_height):
-                if handling == Handling.discard_incomplete_tiles:
+            if(HEIGHT - y < tile.height):
+                if tile.handling == Handling.discard_incomplete_tiles:
                     done_y = True
                     continue
-                if handling == Handling.complete_tiles_shift:
-                    y = HEIGHT - tile_height
+                if tile.handling == Handling.complete_tiles_shift:
+                    y = HEIGHT - tile.height
                 done_y = True
-            output_filename = generate_tile_name_with_coordinates(output_base, x, x_min, x_size, y, y_min, y_size, tile_width, tile_height)
-            command = "gdal_translate -of JPEG -srcwin --config GDAL_PAM_ENABLED NO " + str(x)+ ", " + str(y) + ", " + str(tile_width) + ", " + str(tile_height) + " " + str(tiff_path) + " " + str(output_path) + str(output_filename) + ".jpeg"
+            output_filename = generate_tile_name_with_coordinates(date, x, x_min, x_size, y, y_min, y_size, tile)
+            command = "gdal_translate -of JPEG -srcwin --config GDAL_PAM_ENABLED NO {x}, {y}, {t_width}, {t_height} {tif_path} {out_path}{out_name}.jpeg".format(x=str(x), y=str(y), t_width=tile.width, t_height=tile.height, tif_path=tiff_path, out_path=output_path, out_name=output_filename)
             os.system(command)
             y += y_step
         x += x_step
@@ -167,7 +180,7 @@ def write_to_tfrecords(input_path, output_path):
         version = 0
         while(count < len(files)):
             total_file_size = 0
-            with tf.io.TFRecordWriter(output_path + 'modis_tf' + str(version) + '.tfrecord') as writer:
+            with tf.io.TFRecordWriter("{path}modis_tf{v}.tfrecord".format(path=output_path, v=str(version))) as writer:
                 while(total_file_size < MAX_FILE_SIZE and count < len(files)):    
                     filename = files[count]
                     date = filename[-53:-42]
@@ -182,104 +195,112 @@ def write_to_tfrecords(input_path, output_path):
                     count += 1
             version += 1
 
-def main():
-    parser = ArgumentParser()
-    parser.add_argument("start_date", metavar='start-date', type=str, help="starting date for downloads")
-    parser.add_argument("end_date", metavar='end-date',type=str, help="ending date for downloads")
+def generate_download_path(start_date, end_date, tl_coords, output):
+    base = "modis_{u_lat}_{lft_lon}_{st_date}-{end_date}".format(u_lat=str(round(tl_coords.y, 4)), lft_lon=str(round(tl_coords.x, 4)), st_date=start_date.replace('-',''), end_date=end_date.replace('-', ''))
+    return os.path.join(output, base)
 
-    parser.add_argument("top_left_coords", metavar='top-left-coords', type=str, help="coordinates for top left corner formatted lat,lon (NO SPACE)")
-    parser.add_argument("bottom_right_coords", metavar='bottom-right-coords', type=str, help="coordinates for left longitude formatted lat,lon (NO SPACE)")    
-
-    parser.add_argument("--output-path", default=os.getcwd(), type=str, help="path to output directory")
-
-    parser.add_argument("--tile", default=False, type=bool, help="Tiling flag")
-    parser.add_argument("--tile-width", default=512, type=int, help="tiled image width")
-    parser.add_argument("--tile-height", default=512, type=int, help="tiled image height")
-    parser.add_argument("--tile-overlap", default=0.5, type=float, help="percent overlap for each tile")
-    parser.add_argument("--boundary-handling", default=Handling.complete_tiles_shift, type=Handling, help="define how to handle tiles at image boundaries", choices=list(Handling))
-
-    parser.add_argument("--remove-originals", default=False, type=bool, help="keep/delete original downloaded images")
-    parser.add_argument("--generate-tfrecords", default=False, type=bool, help="generate tfrecords for image tiles")
-    parser.add_argument("--verbose", default=False, type=bool, help="log downloading process")
-    args = parser.parse_args()
-
-    start_date = args.start_date
-    end_date = args.end_date
-
-    output_path = args.output_path
-
-    # get the latitude, longitude values from the user input
-    top_left_coords = [float(i) for i in args.top_left_coords.split(',')]
-    bottom_right_coords = [float(i) for i in args.bottom_right_coords.split(',')]
-    upper_lat = top_left_coords[0]
-    left_lon = top_left_coords[1]
-    lower_lat = bottom_right_coords[0]
-    right_lon = bottom_right_coords[1]
-    
-    # check if inputted coordinates are valid
-    if (right_lon < left_lon or upper_lat < lower_lat):
-        raise argparse.ArgumentTypeError('Inputted coordinates are invalid: order should be (upper latitude,left longitude lower latitude,right longitude)')
-    
-    tiling = args.tile
-    tile_width = args.tile_width
-    tile_height = args.tile_height
-    tile_overlap = args.tile_overlap
-    boundary_handling = args.boundary_handling
-
-    logging = args.verbose
-
-    remove_originals = args.remove_originals
-    write_tfrecords = args.generate_tfrecords
-
-    # gets paths for downloads
-    download_base = "modis_" + str(round(upper_lat, 2)) + "_" + str(round(left_lon, 2)) + "_" + start_date.replace('-','') + '-' + end_date.replace('-', '')
-    download_path = os.path.join(output_path, download_base)
-    originals_path = download_path + '/original_images/'
-    tiled_path = download_path + '/tiled_images/'
-    tfrecords_path = download_path + '/tfrecords/'
-
-    # download the original images if not already downloaded
+def download_originals(download_path, originals_path, tiled_path, tfrecords_path, start_date, end_date, logging, tl_coords, br_coords):
     if not os.path.isdir(download_path):
         os.mkdir(download_path)
         os.mkdir(originals_path)
         os.mkdir(tiled_path)
         os.mkdir(tfrecords_path)
-        dates = pd.date_range(start=args.start_date, end=args.end_date)
+        dates = pd.date_range(start=start_date, end=end_date)
         for date in dates:
             if logging: 
                 print('Downloading:', date)
-            download_area_tiff((upper_lat, left_lon, lower_lat, right_lon), date.strftime("%Y-%m-%d"), originals_path)
+            download_area_tiff((tl_coords.y, tl_coords.x, br_coords.y, br_coords.x), date.strftime("%Y-%m-%d"), originals_path)
+    else:
+        print("The specified region and set of dates has already been downloaded")
 
-    # tile the downloaded images
-    resolution = str(tile_width) + 'x' + str(tile_height) + '_' + str(tile_overlap)
-    tile_res_path = os.path.join(tiled_path, resolution) + '/'
-    if tiling:
-        if not os.path.isdir(tile_res_path):
+def tile_originals(tile_res_path, originals_path, tile, logging):
+    if not os.path.isdir(tile_res_path):
             os.mkdir(tile_res_path)
             for directory, subdirectory, files in os.walk(originals_path):
                 for filename in files:
                     tiff_path = os.path.join(directory, filename)
                     if logging:
                         print("Tiling image at:", tiff_path)
-                    img_to_tiles(tiff_path, tile_width, tile_height, tile_overlap, tile_res_path, boundary_handling)
+                    img_to_tiles(tiff_path, tile, tile_res_path)
+    else:
+        print("The specified tiles for these images have already been generated")
 
-    #write tiles to TFRecords
-    tfrecords_res_path = os.path.join(tfrecords_path, resolution) + '/'
-    if write_tfrecords:
-        if os.path.isdir(tile_res_path):
+def tile_to_tfrecords(tile_res_path, tfrecords_res_path, logging):
+    if os.path.isdir(tile_res_path):
             if not os.path.isdir(tfrecords_res_path):
                 os.mkdir(tfrecords_res_path)
                 if logging: 
                     print("Writing files at:", tile_res_path, " to TFRecords")
                 write_to_tfrecords(tile_res_path, tfrecords_res_path)
-        else: 
-            print("Unable to write to TFRecords due to nonexistent tile path")
+            else:
+                print("The specified TFRecords have already been written")
+    else: 
+        print("Unable to write to TFRecords due to nonexistent tile path")
 
-    if remove_originals:
-        if logging: 
-            print("Removing original images...")
-        shutil.rmtree(originals_path)
-        os.mkdir(originals_path)
+def remove_originals(originals_path, logging):
+    if logging: 
+        print("Removing original images...")
+    shutil.rmtree(originals_path)
+    os.mkdir(originals_path)
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("start_date", metavar='start-date', type=str, help="starting date for downloads")
+    parser.add_argument("end_date", metavar='end-date',type=str, help="ending date for downloads")
+    parser.add_argument("top_left_coords", metavar='top-left-coords', type=str, help="coordinates for top left corner formatted lat,lon (NO SPACE)")
+    parser.add_argument("bottom_right_coords", metavar='bottom-right-coords', type=str, help="coordinates for left longitude formatted lat,lon (NO SPACE)")    
+    parser.add_argument("--output-path", default=os.getcwd(), type=str, help="path to output directory")
+    parser.add_argument("--tile", default=False, type=bool, help="Tiling flag")
+    parser.add_argument("--tile-width", default=512, type=int, help="tiled image width")
+    parser.add_argument("--tile-height", default=512, type=int, help="tiled image height")
+    parser.add_argument("--tile-overlap", default=0.5, type=float, help="percent overlap for each tile")
+    parser.add_argument("--boundary-handling", default=Handling.complete_tiles_shift, type=Handling, help="define how to handle tiles at image boundaries", choices=list(Handling))
+    parser.add_argument("--remove-originals", default=False, type=bool, help="keep/delete original downloaded images")
+    parser.add_argument("--generate-tfrecords", default=False, type=bool, help="generate tfrecords for image tiles")
+    parser.add_argument("--verbose", default=False, type=bool, help="log downloading process")
+
+    # get the user input
+    args = parser.parse_args()
+    start_date = args.start_date
+    end_date = args.end_date
+    output_path = args.output_path
+    logging = args.verbose
+    rm_originals = args.remove_originals
+    write_tfrecords = args.generate_tfrecords
+    tiling = args.tile
+    tile = Tile(args.tile_width, args.tile_height, args.tile_overlap, args.boundary_handling)
+
+    # get the latitude, longitude values from the user input
+    tl_coords = Coordinate([float(i) for i in args.top_left_coords.split(',')])
+    br_coords = Coordinate([float(i) for i in args.bottom_right_coords.split(',')])
+    
+    # check if inputted coordinates are valid
+    if (br_coords.x < tl_coords.x or tl_coords.y < br_coords.y):
+        raise argparse.ArgumentTypeError('Inputted coordinates are invalid: order should be (upper_latitude,left_longitude lower_latitude,right_longitude)')
+    
+    # gets paths for downloads
+    download_path = generate_download_path(start_date, end_date, tl_coords, output_path)
+    originals_path = download_path + '/original_images/'
+    tiled_path = download_path + '/tiled_images/'
+    tfrecords_path = download_path + '/tfrecords/'
+    resolution = "{t_width}x{t_height}_{t_overlap}".format(t_width=str(tile.width), t_height=str(tile.height), t_overlap=str(tile.overlap))
+    tile_res_path = os.path.join(tiled_path, resolution) + '/'
+    tfrecords_res_path = os.path.join(tfrecords_path, resolution) + '/'
+
+    # download the original images if not already downloaded
+    download_originals(download_path, originals_path, tiled_path, tfrecords_path, start_date, end_date, logging, tl_coords, br_coords)
+
+    # tile the downloaded images
+    if tiling:
+        tile_originals(tile_res_path, originals_path, tile, logging)
+
+    # write tiles to TFRecords
+    if write_tfrecords:
+        tile_to_tfrecords(tile_res_path, tfrecords_res_path, logging)
+        
+    # remove original downloaded images
+    if rm_originals:
+        remove_originals(originals_path, logging)
 
 if __name__ == "__main__":
     main()
