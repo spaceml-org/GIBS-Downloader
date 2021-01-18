b5260dcca73e480f1de320e9bbf393d8cc4d732d
fernando <0fernando.lisboa@gmail.com>
2021-01-18 01:25:08 -0600

    Added xml file generation for image download

--- GIBSDownloader/tiff_downloader.py
+++ GIBSDownloader/tiff_downloader.py
@@ -4,7 +4,7 @@ from GIBSDownloader.coordinate_utils import Rectangle, Coordinate
 
 class TiffDownloader():
     @classmethod
-    def download_area_tiff(cls, region, date, output, product):
+    def download_area_tiff(cls, region, date, download_path, xml_path, output, product):
         """
         region: rectangular region to be downloaded
         date: YYYY-MM-DD
@@ -15,9 +15,18 @@ class TiffDownloader():
         width, height = region.calculate_width_height(0.25)
         lon_lat = "{l_x} {upper_y} {r_x} {lower_y}".format(l_x=region.bl_coords.x, upper_y=region.tr_coords.y, r_x=region.tr_coords.x, lower_y=region.bl_coords.y)
 
-        base = "gdal_translate -of GTiff -outsize {w} {h} -projwin {ll} '<GDAL_WMS><Service name=\"TMS\"><ServerUrl>https://gibs.earthdata.nasa.gov/wmts/epsg4326/best/{prod}/default/{d}/250m/".format(w=width, h=height, ll=lon_lat, prod=product.get_long_name(), d=date)
-        end = "${z}/${y}/${x}.jpg</ServerUrl></Service><DataWindow><UpperLeftX>-180.0</UpperLeftX><UpperLeftY>90</UpperLeftY><LowerRightX>396.0</LowerRightX><LowerRightY>-198</LowerRightY><TileLevel>8</TileLevel><TileCountX>2</TileCountX><TileCountY>1</TileCountY><YOrigin>top</YOrigin></DataWindow><Projection>EPSG:4326</Projection><BlockSizeX>512</BlockSizeX><BlockSizeY>512</BlockSizeY><BandsCount>3</BandsCount></GDAL_WMS>' "
+        xml_path = generate_xml(xml_path, product, date)
         filename = "{}{}_{}.tif".format(output, str(product), date)
-        command = base + end + filename
+        command = "gdal_translate -of GTiff -outsize {w} {h} -projwin {ll} {xml} {f}}".format(w=width, h=height, ll=lon_lat, xml=xml_path, f=filename)
+        
         os.system(command)
         return filename
+    
+    @classmethod
+    def generate_xml(cls, xml_path, product, date):
+        xml_base = "<GDAL_WMS><Service name=\"TMS\"><ServerUrl>https://gibs.earthdata.nasa.gov/wmts/epsg4326/best/{prod}/default/{d}/250m/".format(prod=product.get_long_name(), d=date)
+        xml_end = "${z}/${y}/${x}.jpg</ServerUrl></Service><DataWindow><UpperLeftX>-180.0</UpperLeftX><UpperLeftY>90</UpperLeftY><LowerRightX>396.0</LowerRightX><LowerRightY>-198</LowerRightY><TileLevel>8</TileLevel><TileCountX>2</TileCountX><TileCountY>1</TileCountY><YOrigin>top</YOrigin></DataWindow><Projection>EPSG:4326</Projection><BlockSizeX>512</BlockSizeX><BlockSizeY>512</BlockSizeY><BandsCount>3</BandsCount></GDAL_WMS>"
+        xml_content = xml_base + xml_end
+
+        with open('{}{}.xml'.format(xml_path, date), 'w') as xml_file:
+            xml_file.write(xml_content)
