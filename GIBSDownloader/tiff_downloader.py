ac0a76c557ed91f37dc753e6a802eb84e34c409e
fernando <0fernando.lisboa@gmail.com>
2021-01-18 01:32:23 -0600

    Fixed missing xml filename

--- GIBSDownloader/tiff_downloader.py
+++ GIBSDownloader/tiff_downloader.py
@@ -15,9 +15,9 @@ class TiffDownloader():
         width, height = region.calculate_width_height(0.25)
         lon_lat = "{l_x} {upper_y} {r_x} {lower_y}".format(l_x=region.bl_coords.x, upper_y=region.tr_coords.y, r_x=region.tr_coords.x, lower_y=region.bl_coords.y)
 
-        xml_path = generate_xml(xml_path, product, date)
+        xml_filename = generate_xml(xml_path, product, date)
         filename = "{}{}_{}.tif".format(output, str(product), date)
-        command = "gdal_translate -of GTiff -outsize {w} {h} -projwin {ll} {xml} {f}}".format(w=width, h=height, ll=lon_lat, xml=xml_path, f=filename)
+        command = "gdal_translate -of GTiff -outsize {w} {h} -projwin {ll} {xml} {f}}".format(w=width, h=height, ll=lon_lat, xml=xml_filename, f=filename)
         
         os.system(command)
         return filename
@@ -28,5 +28,8 @@ class TiffDownloader():
         xml_end = "${z}/${y}/${x}.jpg</ServerUrl></Service><DataWindow><UpperLeftX>-180.0</UpperLeftX><UpperLeftY>90</UpperLeftY><LowerRightX>396.0</LowerRightX><LowerRightY>-198</LowerRightY><TileLevel>8</TileLevel><TileCountX>2</TileCountX><TileCountY>1</TileCountY><YOrigin>top</YOrigin></DataWindow><Projection>EPSG:4326</Projection><BlockSizeX>512</BlockSizeX><BlockSizeY>512</BlockSizeY><BandsCount>3</BandsCount></GDAL_WMS>"
         xml_content = xml_base + xml_end
 
-        with open('{}{}.xml'.format(xml_path, date), 'w') as xml_file:
+        xml_filename = '{}{}.xml'.format(xml_path, date)
+
+        with open(xml_filename, 'w') as xml_file:
             xml_file.write(xml_content)
+        return xml_filename
