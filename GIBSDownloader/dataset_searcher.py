88b1a80b1ed8379936fe67de199bbd84f2a5cdf6
Navya Sandadi <navya.sandadi@gmail.com>
2021-05-03 12:50:13 -0700

    Modified product search to parse xml file dynamically.

--- GIBSDownloader/dataset_searcher.py
+++ GIBSDownloader/dataset_searcher.py
@@ -1,32 +1,57 @@
 import pathlib
 import sys
 
+import xml.etree.ElementTree as ET
+import urllib.request
+
 import pandas as pd
 
 class DatasetSearcher():
     @classmethod
     def getProductInfo(cls, name):
-        # convert imagery products .csv file into a pandas dataframe
-        path = str(pathlib.Path(__file__).parent.absolute())
-        imagery_products_df = pd.read_csv(path+"/gibs_imagery_products.csv")
-        
-        # search for product name specified by user in available products
-        name = name.lower().split()
-        filter_df = imagery_products_df
+        # XML to parse
+        url = "https://gibs.earthdata.nasa.gov/wmts/epsg4326/best/1.0.0/WMTSCapabilities.xml"
 
+        # Read the xml as a file
+        response = urllib.request.urlopen(url).read()
+        root = ET.fromstring(response)
+
+        # Parse xml to create a list of products
+        imagery_prods = []
+        count = sum(1 for _ in root[3])
+        for i in range(count):
+            for child in root[3][i]:
+                if 'Identifier' in str(child):
+                    product_name = child.text
+                if 'Format' in str(child):
+                    img_format = child.text.replace('image/',"")
+                if 'TileMatrixSetLink' in str(child):
+                    if 'TileMatrixSet' in str(child[0]):
+                        img_res = child[0].text
+            imagery_prods.append([product_name,img_res,img_format])
+
+        # Convert the list of products to pandas dataframe
+        imagery_prods_df = pd.DataFrame(imagery_prods)
+        imagery_prods_df.columns = ['Imagery_Product_Name', 'Image_Resolution', 'Image_Format']
+
+        # Search for product name specified by user in available products
+        name = name.lower().split()
+        filter_df = imagery_prods_df
         for x in name:
             filter_df = filter_df[filter_df.Imagery_Product_Name.str.lower().str.contains(x, na=False)]
-        if len(filter_df) == 1:
+
+        if len(filter_df) == 1 and name[0] == filter_df.Imagery_Product_Name.item():
             name = filter_df.Imagery_Product_Name.item().replace("_"," ")
             res = filter_df.Image_Resolution.item()
-            img_format = filter_df.Format.item()
-
-        elif len(set(filter_df.Imagery_Product_Name)) == 1:
+            img_format = filter_df.Image_Format.item()
+            print(name, res, img_format)
+            
+        elif len(set(filter_df.Imagery_Product_Name)) == 1 and name[0] == set(filter_df.Imagery_Product_Name):
             filter_df = filter_df.sort_values(by=["Image_Resolution"])
             name = filter_df.Imagery_Product_Name.iloc[0].replace("_"," ")
             res = filter_df.Image_Resolution.iloc[0]
-            img_format = filter_df.Format.iloc[0]
-
+            img_format = filter_df.Image_Format.iloc[0]
+            
         else:
             print("\n\n\nPlease enter the full imagery product name from the following list:\n")
             print(filter_df[["Imagery_Product_Name", "Image_Resolution"]].to_string(index=False))
