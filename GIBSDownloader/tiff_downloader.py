3711596e0fb3391327e9f5e010647d45e5de2d9a
vincentsarago <vincent.sarago@gmail.com>
2021-07-08 00:02:29 +0200

    use rasterio instead of GDAL

--- GIBSDownloader/tiff_downloader.py
+++ GIBSDownloader/tiff_downloader.py
@@ -4,6 +4,11 @@ from datetime import date, timedelta
 from GIBSDownloader.product import Product
 from GIBSDownloader.coordinate_utils import Rectangle, Coordinate
 
+import rasterio
+from rasterio.io import MemoryFile
+from rasterio.transform import from_bounds
+
+
 class TiffDownloader():
 
     @classmethod
@@ -12,14 +17,38 @@ class TiffDownloader():
 
     @classmethod
     def download_area_tiff(cls, region, date, xml_path, filename, name, res, img_format, width=None, height=None):
-        
+
         if width == None and height == None:
             width, height = region.calculate_width_height(res)
-        lon_lat = "{l_x} {upper_y} {r_x} {lower_y}".format(l_x=region.bl_coords.x, upper_y=region.tr_coords.y, r_x=region.tr_coords.x, lower_y=region.bl_coords.y)
-        
+
         xml_filename = TiffDownloader.generate_xml(xml_path, name, date)
-        command = "gdal_translate -of {of} -outsize {w} {h} -projwin {ll} {xml} {f}.{ext}".format(of=img_format.upper(), w=width, h=height, ll=lon_lat, xml=xml_filename, f=filename, ext=img_format)
-        os.system(command)
+
+        with rasterio.open(xml_filename) as src:
+            wind = src.window(
+                region.bl_coords.x,
+                region.bl_coords.y,
+                region.tr_coords.x,
+                region.tr_coords.y,
+                precision=21
+            )
+            profile = src.profile
+            profile["driver"] = img_format
+            profile["width"] = width
+            profile["height"] = height
+            if src.crs is not None:
+                profile["transform"] = from_bounds(
+                    region.bl_coords.x,
+                    region.bl_coords.y,
+                    region.tr_coords.x,
+                    region.tr_coords.y,
+                    width,
+                    height,
+                )
+
+            with rasterio.open(filename, "w", **profile) as dst_src:
+                dst_src.write(
+                    src.read(window=wind, out_shape=(src.count, height, width))
+                )
 
     @classmethod
     def get_dates_range(cls, start_date, end_date):
@@ -31,7 +60,7 @@ class TiffDownloader():
 
         dates = [d1 + timedelta(days=x) for x in range((d2 - d1).days + 1)]
         return dates
-    
+
     @classmethod
     def generate_xml(cls, xml_path, name, date):
         xml_base = '<GDAL_WMS><Service name="TiledWMS"><ServerUrl>https://gibs.earthdata.nasa.gov/twms/epsg4326/best/twms.cgi?'
