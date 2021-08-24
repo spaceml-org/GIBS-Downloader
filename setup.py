60b368e10eda405fbb6051f4d9bc2ed1e1e46bcd
fernando <0fernando.lisboa@gmail.com>
2021-08-23 19:25:49 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -3,31 +3,39 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as f:
     requirements = f.readlines() 
 
-long_description = '''Python wrapper package for NASA GIBS API \ 
-    with additional utility functions to prepare downloaded images for \
+long_description = '''Python package for downloading NASA GIBS imagery \ 
+    with additional functionality to prepare downloaded images for \
     machine learning pipeline''' 
 
 setup(
-    name ='GIBSDownloader', 
-    version ='1.0.0', 
-    author ='Fernando Lisboa, Shivam Verma', 
-    author_email ='0fernando.lisboa@gmail.com', 
-    url ='https://github.com/spaceml-org/NASA-GIBS-Downloader', 
-    description ='Downloading tool for NASA GIBS', 
-    long_description = long_description, 
-    long_description_content_type ="text/markdown",  
-    packages = find_packages(), 
-    entry_points ={ 
+    name='GIBSDownloader', 
+    version='1.0.0', 
+    license='Apache License 2.0',
+    author='Fernando Lisboa, Navya Sandadi, Shivam Verma', 
+    author_email='0fernando.lisboa@gmail.com', 
+    url='https://github.com/spaceml-org/GIBS-Downloader', 
+    description='Downloading tool for NASA GIBS satellite imagery', 
+    long_description=long_description, 
+    long_description_content_type="text/markdown",  
+    packages=find_packages(), 
+    entry_points={ 
         'console_scripts': [ 
             'gdl = GIBSDownloader.gibs_downloader:main'
         ] 
     }, 
-    classifiers =( 
-        "Programming Language :: Python :: 3", 
+    classifiers=( 
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: Apache Software License',
+        'Intended Audience :: Science/Research',
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Operating System :: OS Independent", 
     ), 
-    keywords ='GIBS gdl satellite python package GIBSDownloader', 
-    install_requires = requirements, 
-    zip_safe = False,
+    keywords='GIBS gdl satellite python package GIBSDownloader', 
+    install_requires=requirements, 
+    python_requires='>=3.6',
+    zip_safe=False,
     include_package_data=True
 )
